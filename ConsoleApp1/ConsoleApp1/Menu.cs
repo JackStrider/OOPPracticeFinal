663872808811fa7f89e010d7bc9f308e52a5b64d
JackStrider <rossi.william@gmail.com>
2017-12-05 11:33:18 -0600

    Everything up to the flavor

---
+++ ConsoleApp1/ConsoleApp1/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Menu
+    {
+        public static void menu()
+        {
+            String Flavor;
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("As you read the words the book, they somehow begin to narrate itself.\n");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Lets begin with what flavor your in the mood for");
+            Console.WriteLine("We have:");
+            Console.WriteLine("1. Original Vanilla");
+            Console.WriteLine("2. Chocolate");
+            Console.WriteLine("3. Deluxe Red Velvet");
+            Console.WriteLine("4. Limited Edition Espresso Supreme/n");
+            Console.WriteLine("Just say the number you want, I'm magic, I can hear you.");
+
+            Flavor = Console.ReadLine();
+            switch(Flavor)
+            {
+                case "1":
+                    {
+                        Flavor = "Original Vanilla";
+                    break;
+                    }
+                case "2":
+                    {
+                        Flavor = "Chocolate";
+                        break;
+                    }
+                case "3":
+                    {
+                        Flavor = "Deluxe Red Velvet";
+                        break;
+                    }
+                case "4":
+                    {
+                        Flavor = "Limited Edition Espresso Supreme";
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/WelcomeMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class WelcomeMessage
+    {
+        public static void welcomeMessage()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("You walk into a dark ally.");
+            Console.WriteLine("Checking over your shoulder you open a steel door in the shadows.");
+            Console.WriteLine("As you walk down a dark staircase you see a light at the bottom.");
+            Console.WriteLine("Strange music tickles your ears and you are drawn in.");
+            Console.WriteLine("Walking through the door at the bottom, you are met by a strange looking man.");
+            Console.WriteLine("His beard somehow manages to allways have a small trickle of smoke");
+            Console.WriteLine("gently wisping out of it. He grins and says:\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Welcome to Wizbolts Discount CupCake Shop!");
+            Console.WriteLine("I'm William Wizbolt, I shall be the guide on your cupcake adventure today.");
+            Console.WriteLine("You can call me Wizbolt though. Lets get you a menu!\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Wizbolt turns around and as he walks behind the counter");
+            Console.WriteLine("you see a massive pile of grey matted hair tied up on the back of his head.\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Your not one of those adventurer tipes are you?\n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Wizbolt squints at you skeptically.");
+            Console.WriteLine("He pulls a massive book from behind the counter \nwith the title 'Cover Businesses'");
+            Console.WriteLine("You walk over as he flips through the pages muttering to himself.");
+            Console.WriteLine("While you walk, you turn and look through a gap in the curtan to the back room");
+            Console.WriteLine("but before your eyes could focus a force caused your head to snap forward.\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Ah! Here it is!\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press any key to read the menu.");
+            Console.ReadKey();
+        }
+    }
+}
